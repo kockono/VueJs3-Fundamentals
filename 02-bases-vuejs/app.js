3291059a4574469f7bc2d46ed90efbe2de7cbb85
kockono <kockonoop@gmail.com>
2022-10-24 05:39:13 -0600

    hola mundo vue

---
+++ 02-bases-vuejs/app.js
@@ -0,0 +1,11 @@
+// Tenemos que especificar en donde queremos renderizar nuestra apliación
+const app = Vue.createApp({
+    // template: `
+    // <h1> Hola Mundo</h1>
+    // <p>Desde app.js</p>
+    // `
+    
+})
+
+console.log(`Vue`, Vue)
+app.mount('#myApp')
