03c5a61c84441ec23c4be92dfd97b0c7657e5988
kockono <kockonoop@gmail.com>
2022-10-28 08:51:26 -0600

    counter

--- 03-fundamentos/src/App.vue
+++ 03-fundamentos/src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
+  <Counter />
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Counter from './components/Counter.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Counter
   }
 }
 </script>
---
+++ 03-fundamentos/src/components/Counter.vue
@@ -0,0 +1,28 @@
+<template>
+  <!-- <h2>Counter</h2> -->
+    <p> Contador: {{ getSquareValue() }}</p>
+</template>
+
+<script>
+export default {
+    name: 'Contador',
+    data() {
+        return {
+            counter: 5
+        }
+    },
+    methods: {
+        getSquareValue(){ 
+            console.log( 'Resultado' )
+            return this.counter * this.counter;
+        }
+    },
+    computed: {
+        
+    }
+}
+</script>
+
+<style>
+
+</style>
