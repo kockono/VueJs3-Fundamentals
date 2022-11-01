ad49cdb80a2ec72bb37e9b2b6354bd1a9ea99003
kockono <kockonoop@gmail.com>
2022-11-01 01:19:49 -0600

    Computed example & Events/Actions

--- 03-fundamentos/src/App.vue
+++ 03-fundamentos/src/App.vue
@@ -1,6 +1,9 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <Counter />
+
+  <div>
+      <img alt="Vue logo" src="./assets/logo.png">
+      <Counter />
+  </div>
 
 </template>
 
--- 03-fundamentos/src/components/Counter.vue
+++ 03-fundamentos/src/components/Counter.vue
@@ -1,6 +1,13 @@
 <template>
-  <!-- <h2>Counter</h2> -->
-    <p> Contador: {{ getSquareValue() }}</p>
+    <div>
+      <h2>Counter</h2>
+    <p>{{ counter }} Contador: {{ squareCounter }}</p>
+    <p>{{ counter }} Contador: {{ squareCounter }}</p>
+    <p>{{ counter }} Contador: {{ squareCounter }}</p>
+    <p>{{ counter }} Contador: {{ squareCounter }}</p>
+    <button v-on:click="sumarCounter()">+ 1</button>
+    <button v-on:click="restarCounter()">- 1</button>
+  </div>
 </template>
 
 <script>
@@ -15,14 +22,31 @@ export default {
         getSquareValue(){ 
             console.log( 'Resultado' )
             return this.counter * this.counter;
+        },
+        sumarCounter(){
+            this.counter++;
+        },
+        restarCounter(){
+            this.counter--;
         }
+
     },
     computed: {
-        
+        squareCounter() {
+            console.log( 'Resultado' )
+            return this.counter * this.counter
+        }
     }
 }
 </script>
 
 <style>
+
+    button {
+        /* margin: 0 5px; */
+        background-color: #64B687;
+        /* border: 1px; */
+        /* margin: 0 5px; */
+    }
 
 </style>
